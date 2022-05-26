0260458b941ddbff9f873f2843972008b7d38c7d
aleksaskrbic <64932203+aleksaskrbic@users.noreply.github.com>
2022-05-26 13:07:25 +0200

    Add files via upload

--- Web_Projekat/src/main/resources/data.sql
+++ Web_Projekat/src/main/resources/data.sql
@@ -1,45 +1,45 @@
 --Dodavanje Korisnika
 INSERT INTO KORISNIK(DTYPE, username,lozinka, ime, prezime, pol, datum_rodjenja, role)
-VALUES ('Administrator' , 'lukasenko','belorusija','ALeksandar', 'Lukasenko', 'Muski', '02.05.1976.', 0);
+VALUES ('Korisnik' , 'lukasenko','belorusija','ALeksandar', 'Lukasenko', 'Muski', '02.05.1976.', 0);
 
 INSERT INTO KORISNIK(DTYPE, username,lozinka, ime, prezime, pol, datum_rodjenja, role)
-VALUES ('Dostavljac' , 'milance','milance','Milan', 'Milkic', 'Muski', '02.05.1986.', 1);
+VALUES ('Korisnik' , 'milance','milance','Milan', 'Milkic', 'Muski', '02.05.1986.', 1);
 
 INSERT INTO KORISNIK(DTYPE, username,lozinka, ime, prezime, pol, datum_rodjenja, role)
-VALUES ('Dostavljac' , 'nikola123','dostava','Nikola', 'Nikolic', 'Muski', '12.04.1968.', 1);
+VALUES ('Korisnik' , 'nikola123','dostava','Nikola', 'Nikolic', 'Muski', '12.04.1968.', 1);
 
 INSERT INTO KORISNIK(DTYPE, username,lozinka, ime, prezime, pol, datum_rodjenja, role)
-VALUES ('Menadzer' , 'misa','menadzer','Misa', 'Miskovic', 'Muski', '02.07.1996.', 3);
+VALUES ('Korisnik' , 'misa','menadzer','Misa', 'Miskovic', 'Muski', '02.07.1996.', 3);
 
 INSERT INTO KORISNIK(DTYPE, username,lozinka, ime, prezime, pol, datum_rodjenja, role)
-VALUES ('Menadzer' , 'srki123','menadzer','Srdjan', 'Srdanovic', 'Muski', '02.5.1993.', 3);
+VALUES ('Korisnik' , 'srki123','menadzer','Srdjan', 'Srdanovic', 'Muski', '02.5.1993.', 3);
 
 INSERT INTO KORISNIK(DTYPE, username,lozinka, ime, prezime, pol, datum_rodjenja, role, broj_sakupljenih_poena, tip_kupca)
-VALUES ('Kupac' , 'kupac1','kupovina','Aleksa', 'Milanovic', 'Muski', '03.04.1996.', 2, 25, 0);
+VALUES ('Korisnik' , 'kupac1','kupovina','Aleksa', 'Milanovic', 'Muski', '03.04.1996.', 2, 25, 0);
 
 INSERT INTO KORISNIK(DTYPE, username,lozinka, ime, prezime, pol, datum_rodjenja, role, broj_sakupljenih_poena, tip_kupca)
-VALUES ('Kupac' , 'kupac2','kupovina2','Zeljko', 'Zelenski', 'Muski', '03.06.1976.', 2, 0, 3);
+VALUES ('Korisnik' , 'kupac2','kupovina2','Zeljko', 'Zelenski', 'Muski', '03.06.1976.', 2, 0, 3);
 
 INSERT INTO KORISNIK(DTYPE, username,lozinka, ime, prezime, pol, datum_rodjenja, role, broj_sakupljenih_poena, tip_kupca)
-VALUES ('Kupac' , 'kupac3','kupovina3','Vladimir', 'Putin', 'Muski', '03.07.1986', 2, 0, 1);
-
---Dodavanje Artikala
-
-INSERT INTO ARTIKAL(naziv, tip_hrane, kolicina, opis)
-VALUES ('Origano', 0, 12,'Zacin');
-
-INSERT INTO ARTIKAL(naziv, tip_hrane, kolicina, opis)
-VALUES ('Piletina',0, 25, 'Hrana');
-
-INSERT INTO ARTIKAL(naziv, tip_hrane, kolicina, opis)
-VALUES ('Pepsi',1 , 12,'Pice.');
+VALUES ('Korisnik' , 'kupac3','kupovina3','Vladimir', 'Putin', 'Muski', '03.07.1986', 2, 0, 1);
 
 --Dodavanje Restorana
-INSERT INTO RESTORAN(Id, Naziv, tip_restorana, Lokacija,menadzer)
-VALUES(1,'Pizza-bar',1,'Skadarska 3', 4);
+INSERT INTO RESTORAN(Naziv, tip_restorana, status, Lokacija,menadzer)
+VALUES('Pizza-bar','Italijanski',0, 'Skadarska 23', null);
 
-INSERT INTO RESTORAN(Id, Naziv, tip_restorana, Lokacija, menadzer)
-VALUES(2,'Srbija',2,'Skadarska 18', 4);
+INSERT INTO RESTORAN(Naziv, tip_restorana, status, Lokacija, menadzer)
+VALUES('Srbija','Srpski',1, 'Dobricina 18', null);
+--Dodavanje Artikala
+
+INSERT INTO ARTIKAL(naziv, tip_hrane, kolicina, opis,cena,restoran_id)
+VALUES ('Origano', 'Jelo', 12,'Zacin', 15, 1);
+
+INSERT INTO ARTIKAL(naziv, tip_hrane, kolicina, opis, cena, restoran_id)
+VALUES ('Piletina','Jelo', 25, 'Hrana', 225,null);
+
+INSERT INTO ARTIKAL(naziv, tip_hrane, kolicina, opis, cena, restoran_id)
+VALUES ('Pepsi','Pice' , 12,'Pice.', 10,null);
+
 
 --Dodavanje Komentara
 INSERT INTO KOMENTAR(id_kupca, id_restorana, komentar, ocena)
@@ -47,6 +47,12 @@ VALUES('6', 1, 'Fenomenalna usluga.', 5 );
 
 INSERT INTO KOMENTAR(id_kupca, id_restorana, komentar, ocena)
 VALUES('7', 2, 'Jako losa usluga.', 1 );
+
+INSERT INTO LOKACIJA(geografska_duzina,geografska_sirina, adresa )
+VALUES(22.23, 25.64, 'Sarajevska 123');
+
+INSERT INTO LOKACIJA(geografska_duzina,geografska_sirina, adresa )
+VALUES(42.44, 45.64, 'Novosadska 18');
 
 
 
