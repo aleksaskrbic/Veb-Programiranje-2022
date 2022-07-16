300252be7eb5488ea260c7910b0f65481ba405a4
aleksaskrbic <64932203+aleksaskrbic@users.noreply.github.com>
2022-07-16 14:52:07 +0200

    Add files via upload

--- Web_Projekat/src/main/java/com/example/Web_Projekat/WebProjekatApplication.java
+++ Web_Projekat/src/main/java/com/example/Web_Projekat/WebProjekatApplication.java
@@ -2,11 +2,19 @@ package com.example.Web_Projekat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = {"com.example.Web_Projekat.rest.controller" })
+
 public class WebProjekatApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebProjekatApplication.class, args);
 	}
+	
+	
+
 }
